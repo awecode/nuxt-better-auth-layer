351fa88b13dd3eb3a60d1c9a0214661a6a188ac9
Dipesh Acharya <dipesh@awecode.com>
2025-08-23 17:39:18 +0545

    refactor: move server auth config to a different file for ease of editing when extending the layer locally

---
+++ server/utils/auth.config.ts
@@ -0,0 +1,45 @@
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { magicLink, bearer, admin } from 'better-auth/plugins'
+// import { createAuthMiddleware } from 'better-auth/api'
+// import { allowDomains, allowEmails, setAdminForEmail } from '../lib/hook-utils'
+import { useDb } from '../../../../server/utils/db'
+
+import { sendMagicLinkEmail } from './email'
+import type { BetterAuthOptions } from 'better-auth'
+
+export const betterAuthConfig: BetterAuthOptions = {
+  emailAndPassword: {
+    enabled: false,
+  },
+  plugins: [
+    bearer(),
+    admin(),
+    magicLink({
+      sendMagicLink: async ({ email, token, url }, request) => {
+        await sendMagicLinkEmail(email, token, url, request)
+      },
+    }),
+  ],
+  database: drizzleAdapter(useDb(), {
+    provider: 'sqlite',
+    usePlural: true,
+  }),
+  // hooks: {
+  //   before: createAuthMiddleware(async (ctx) => {
+  //     // Only allow emails from the domains as users
+  //     allowDomains(ctx)
+  //     // Only allow particular emails as users
+  //     allowEmails(ctx)
+  //   }),
+  // },
+  // databaseHooks: {
+  //   user: {
+  //     create: {
+  //       before: async (user) => {
+  //         // Automatically set admin role for particular emails
+  //         return setAdminForEmail(user)
+  //       },
+  //     },
+  //   },
+  // },
+}
--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,46 +1,13 @@
 import { betterAuth } from 'better-auth'
-import { drizzleAdapter } from 'better-auth/adapters/drizzle'
-// import { createAuthMiddleware } from 'better-auth/api'
-import { magicLink, bearer, admin } from 'better-auth/plugins'
+import { betterAuthConfig } from './auth.config'
 // import { runtime } from 'std-env'
-import { useDb } from '../../../../server/utils/db'
-// import { allowDomains, allowEmails, setAdminForEmail } from '../lib/hook-utils'
-import { sendMagicLinkEmail } from './email'
 
-export const createBetterAuth = () => betterAuth({
-  emailAndPassword: {
-    enabled: false,
-  },
+const authConfig = {
+  ...betterAuthConfig,
   baseURL: getBaseURL(),
-  plugins: [
-    bearer(),
-    admin(),
-    magicLink({
-      sendMagicLink: async ({ email, token, url }, request) => {
-        await sendMagicLinkEmail(email, token, url, request)
-      },
-    }),
-  ],
-  database: drizzleAdapter(useDb(), {
-    provider: 'sqlite',
-    usePlural: true,
-  }),
-  // hooks: {
-  //   before: createAuthMiddleware(async (ctx) => {
-  //     allowDomains(ctx)
-  //     // allowEmails(ctx)
-  //   }),
-  // },
-  // databaseHooks: {
-  //   user: {
-  //     create: {
-  //       before: async (user) => {
-  //         return setAdminForEmail(user)
-  //       },
-  //     },
-  //   },
-  // },
-})
+}
+
+export const createBetterAuth = () => betterAuth(authConfig)
 
 let _auth: ReturnType<typeof createBetterAuth>
 // For better-auth cli to generate schema
